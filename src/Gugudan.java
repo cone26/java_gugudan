4acc744685fdba82a4f4ff0f2de7fc5727336737
Siah <72402014+cone26@users.noreply.github.com>
2022-01-21 16:02:23 +0900

    4단과 5단

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,28 +1,64 @@
-
+import java.util.Scanner; 
 public class Gugudan {
 	public static void main(String[] args) {
-		//2단
-		System.out.println("2단");
-		System.out.println(2 * 1);
-		System.out.println(2 * 2);
-		System.out.println(2 * 3);
-		System.out.println(2 * 4);
-		System.out.println(2 * 5);
-		System.out.println(2 * 6);
-		System.out.println(2 * 7);
-		System.out.println(2 * 8);
-		System.out.println(2 * 9);
+//		//2단
+//		System.out.println("-----2단-----");
+//		System.out.println(2 * 1);
+//		System.out.println(2 * 2);
+//		System.out.println(2 * 3);
+//		System.out.println(2 * 4);
+//		System.out.println(2 * 5);
+//		System.out.println(2 * 6);
+//		System.out.println(2 * 7);
+//		System.out.println(2 * 8);
+//		System.out.println(2 * 9);
+//		
+//		//3단
+//		System.out.println("-----3단-----");
+//		System.out.println(3 * 1);
+//		System.out.println(3 * 2);
+//		System.out.println(3 * 3);
+//		System.out.println(3 * 4);
+//		System.out.println(3 * 5);
+//		System.out.println(3 * 6);
+//		System.out.println(3 * 7);
+//		System.out.println(3 * 8);
+//		System.out.println(3 * 9);
 		
-		//3단
-		System.out.println("3단");
-		System.out.println(3 * 1);
-		System.out.println(3 * 2);
-		System.out.println(3 * 3);
-		System.out.println(3 * 4);
-		System.out.println(3 * 5);
-		System.out.println(3 * 6);
-		System.out.println(3 * 7);
-		System.out.println(3 * 8);
-		System.out.println(3 * 9);
+		//4단 변수사용
+		System.out.println("-----4단-----");
+		int result = 4 * 1;
+		System.out.println(result);
+		result = 4 * 2;
+		System.out.println(result);
+		result = 4 * 3;
+		System.out.println(result);
+		result = 4 * 4;
+		System.out.println(result);
+		result = 4 * 5;
+		System.out.println(result);
+		result = 4 * 6;
+		System.out.println(result);
+		result = 4 * 7;
+		System.out.println(result);
+		result = 4 * 8;
+		System.out.println(result);
+		result = 4 * 9;
+		System.out.println(result);
+		
+		//5단 사용자에게 입력 + 변수사용
+		System.out.println("몇 단을 계산할까요? : ");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		System.out.println("-----"+num+"단-----");
+		System.out.println(num * 1);
+		System.out.println(num * 2);
+		System.out.println(num * 3);
+		System.out.println(num * 4);
+		System.out.println(num * 5);
+		System.out.println(num * 6);
+		System.out.println(num * 7);
+		System.out.println(num * 8);
+		System.out.println(num * 9);
 	}
 }
