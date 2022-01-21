d0a2801028115ab677f2d0d4bc87015176ea9f92
Siah <72402014+cone26@users.noreply.github.com>
2022-01-21 19:26:32 +0900

    배열과 반복문을 이용한 구구단 출력

---
+++ src/gugudan_array.java
@@ -0,0 +1,16 @@
+
+public class gugudan_array {
+	public static void main(String[] args) {
+		//배열을 이용해서 구구단 출력하기 + 2중반복문
+		int[] result = new int[9];
+		for(int i = 2; i < 10; i ++) {
+			System.out.println("-----" + i + "단-----");
+			for(int j = 0; j < result.length; j++) {
+				result[j] = i * (j+1);
+			}
+			for (int j = 0; j < result.length; j ++) {
+				System.out.println(i + "*" + (j+1) + " = "+ result[j]);
+			}
+		}
+	}
+}
